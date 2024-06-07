23913925fa13ea41104bc2f9b7107dfd5d94123d
felix <1534589097@qq.com>
2024-06-07 23:56:19 +0800

    modify main

--- __main__.py
+++ __main__.py
@@ -1,13 +1,13 @@
 import sys
 from gui import mainwindow
-from kiloncore import utils, consts
+
 # 错误处理
 def exception_hook(exc_type, exc_value, exc_traceback):
    pass
 
 if __name__ == '__main__':
-    sys.excepthook = exception_hook
-    mainwindow.Start()
-    #utils.getcharfromimage(consts.cellActive)
+   sys.excepthook = exception_hook
+   
+   mainwindow.Start()
 
 
